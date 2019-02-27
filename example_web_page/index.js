c4421aa11d5e7783b70bedcba0324a118150120c
Adriano Dennanni <adriano.dennanni@infosimples.com.br>
2019-02-27 16:37:45 -0300

    Done

--- example_web_page/index.js
+++ example_web_page/index.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
   serverlessURL = 'https://us-central1-neuronio-edp.cloudfunctions.net/teste-03';
 
-  var canvas = document.createElement('canvas');
-  canvas.width = 96;
-  canvas.height = 96;
-  var ctx = canvas.getContext("2d");
+  const toDataURL = url => fetch(url).then(
+    response => response.blob()).then(
+    blob => new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(blob)
+    }));
 
-  // [
-  //   '10037', '11640', '14739', '15913', '21253', '22383', '23516', '2958',
-  //   '31562', '6827', '10978', '11646', '15184', '19625', '22285', '23472',
-  //   '28947', '30915', '4693', '7974'
-  // ].forEach(addCard);
   [
-    '10037',
+    '10037', '11640', '14739', '15913', '21253', '22383', '23516', '2958',
+    '31562', '6827', '10978', '11646', '15184', '19625', '22285', '23472',
+    '28947', '30915', '4693', '7974'
   ].forEach(addCard);
 
   function addCard(cell, index, array) {
@@ -29,40 +30,31 @@ $(document).ready(function() {
     );
     var img = $(`#${cell}`).children()[0];
 
-    const toDataURL = url => fetch(url)
-    .then(response => response.blob())
-    .then(blob => new Promise((resolve, reject) => {
-      const reader = new FileReader()
-      reader.onloadend = () => resolve(reader.result)
-      reader.onerror = reject
-      reader.readAsDataURL(blob)
-    }))
-
-    toDataURL(img.src)
-    .then(dataUrl => {
-      console.log('RESULT:', dataUrl)
-    })
-
-    // img.onload = function() {
-    //   ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, 96, 96);
-    //   imageData = ctx.getImageData(0, 0, 224, 224);
-    //   var dataURL = canvas.toDataURL();
-    //   // dataURL = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-    //   // dataURL = dataURL + "=".repeat((4 - (dataURL.length % 4)) % 4);
-    //   console.log(dataURL)
-    //   axios({
-    //     method: 'post',
-    //     url: serverlessURL,
-    //     data: Qs.stringify({
-    //       image_base64: dataURL
-    //     }),
-    //     headers: {
-    //       'Accept': 'application/x-www-form-urlencoded',
-    //       'Content-Type': 'application/x-www-form-urlencoded'
-    //     }
-    //   }).then(function(response) {
-    //     console.log(`${cell} - ${response}`);
-    //   });
-    // }
+    toDataURL(`https://github.com/adrianodennanni/serverless_deep_learning_deploy_example/raw/master/example_web_page/cells/${cell}.png`)
+      .then(dataUrl => {
+        axios({
+          method: 'post',
+          url: serverlessURL,
+          data: Qs.stringify({
+            image_base64: dataUrl.replace(/^data:image\/(png|jpg);base64,/, "")
+          }),
+          headers: {
+            'Accept': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }).then(function(response) {
+          var data = JSON.parse(response.data.replace(/'/g,"\""));
+          var parasitized = data['Parasitized'];
+          var uninfected = data['Uninfected'];
+          console.log()
+          if (parasitized > uninfected) {
+            $(`#${cell} div.result`).css("background-color", "red");
+            $(`#${cell} div.result`).text('Parasitized');
+          } else {
+            $(`#${cell} div.result`).css("background-color", "green");
+            $(`#${cell} div.result`).text('Uninfected');
+          }
+        });
+      });
   };
 });
